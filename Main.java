28df6ac501bef94d2f998244873a2553be8bdcf4
bedek8184 <bedek8184@wrdsb.ca>
2021-02-18 16:50:41 +0000

    Change to birth year and name

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * The Hello World Prgram
  * @author Krishna Bedessee 
@@ -9,7 +10,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    System.out.println("Hello Krishna");
-    
+    // Ask the user for their name
+    System.out.println("Please enter your name:");
+   
+   // creating a Scanner for user input
+   Scanner input = new Scanner(System.in);
+
+    // declare and initialize the name variable
+    String name = input.nextLine();
+    // say hello to the user
+    System.out.println("Hello " + name);
+
+    // ask the user the year they were born
+    System.out.println("Please enter the year you were born:");
+
+    // create the variable to store the birth year
+    int birthYear = input.nextInt();
+
+    // calculate the approximate age
+    final int CURRENT_YEAR = 2021;
+    int age = CURRENT_YEAR - birthYear;
+
+    // let the user know their age
+    System.out.println("You are approximately " + age + " years old.");
   }
 }
