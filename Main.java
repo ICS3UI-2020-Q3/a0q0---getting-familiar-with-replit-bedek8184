d203309a5b47932b1eafe58505dc6c1a7c4281e4
bedek8184 <bedek8184@wrdsb.ca>
2021-02-18 15:50:40 +0000

    Completed Hello World

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ * The Hello World Prgram
+ * @author Krishna Bedessee 
  */
 public class Main {
 
@@ -9,7 +9,7 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    System.out.println("Hello Krishna");
     
   }
 }
